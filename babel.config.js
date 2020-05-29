1ba3baa852e071e5452a727549efbb77ebbbf584
anatolyefimov <efimo.tolik@gmail.com>
2020-05-30 01:33:37 +0300

    fix

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,13 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        "plugins": [
+        'plugins': [
             [
-                "module-resolver",
+                'module-resolver',
                 {
-                    "root": ["./src"],
-                    "alias": {
-                        "@": "./src"
+                    'root': ['./src'],
+                    'alias': {
+                        '@': './src'
                     }
                 }
             ]
